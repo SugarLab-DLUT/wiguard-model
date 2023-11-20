c570eb7cc5fa679775fd0ef81e085f69d96d37a9
Saurlax <saurlax@qq.com>
2023-11-20 21:10:04 +0800

    feat: separate pars

--- client/send.sh
+++ client/send.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+send_delay=5s
+packet_delay=0.2s
+server_ip='192.168.3.35'
+gateway_ip='192.168.3.1'
+disconnect_cmd='sudo modprobe -r iwlwifi mac80211'
+reconnect_cmd='sudo modprobe iwlwifi connector_log=0x1'
+log_to_file_path='~/linux-80211n-csitool-supplementary/netlink/log_to_file'
+
 echo disconnecting
-sudo modprobe -r iwlwifi mac80211
+$disconnect_cmd
 sleep 5s
 echo reconnecting
-sudo modprobe iwlwifi connector_log=0x1
+$reconnect_cmd
 sleep 10s
-ping 192.168.3.1 -i 0.2 &
+ping $gateway_ip -i $packet_delay &
 
 while true
 do
-~/linux-80211n-csitool-supplementary/netlink/log_to_file csi-temp.dat &
-sleep 5s
+$log_to_file_path &
+sleep $delay
 kill $!
 cat csi-temp.dat
-cat csi-temp.dat > /dev/tcp/192.168.3.35/1212
+cat csi-temp.dat > /dev/tcp/$server_ip/1212
 done
